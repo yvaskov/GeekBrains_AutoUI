fd3436d86c9a3508ad7b158cae1c6f2a6073be21
yvaskov <y.vaskov@inbox.ru>
2020-11-16 14:29:35 +0300

    Initial commit

---
+++ src/main/java/GeekBrains_AutoUI/App.java
@@ -0,0 +1,13 @@
+package GeekBrains_AutoUI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
